33c9d183f6e34832eaa8de930753c319b32a99a4
LvffY <louberger@hotmail.fr>
2022-02-20 17:13:27 +0000

    ✨ Fix ansible install method

--- pkg/ansible/build.go
+++ pkg/ansible/build.go
@@ -1,6 +1,8 @@
 package ansible
 
 import (
+	"fmt"
+
 	"get.porter.sh/porter/pkg/exec/builder"
 	yaml "gopkg.in/yaml.v2"
 )
@@ -18,18 +20,6 @@ type BuildInput struct {
 type MixinConfig struct {
 	ClientVersion string `yaml:"clientVersion,omitempty"`
 }
-
-// This is an example. Replace the following with whatever steps are needed to
-// install required components into
-// const dockerfileLines = `RUN apt-get update && \
-// apt-get install gnupg apt-transport-https lsb-release software-properties-common -y && \
-// echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ stretch main" | \
-//    tee /etc/apt/sources.list.d/azure-cli.list && \
-// apt-key --keyring /etc/apt/trusted.gpg.d/Microsoft.gpg adv \
-// 	--keyserver packages.microsoft.com \
-// 	--recv-keys BC528686B50D79E339D3721CEB3E94ADBE1229CF && \
-// apt-get update && apt-get install azure-cli
-// `
 
 // Build will generate the necessary Dockerfile lines
 // for an invocation image using this mixin
@@ -52,10 +42,11 @@ func (m *Mixin) Build() error {
 		m.ClientVersion = suppliedClientVersion
 	}
 
-	//fmt.Fprintf(m.Out, dockerfileLines)
-
 	// Example of pulling and defining a client version for your mixin
-	// fmt.Fprintf(m.Out, "\nRUN curl https://get.helm.sh/helm-%s-linux-amd64.tar.gz --output helm3.tar.gz", m.ClientVersion)
+	fmt.Fprintf(m.Out, `RUN pip install --upgrade --no-cache-dir pip && \
+    pip install --upgrade --no-cache-dir setuptools wheel && \
+    pip install --upgrade --no-cache-dir 'ansible%s'
+    `, m.ClientVersion)
 
 	return nil
 }
---
+++ pkg/ansible/build_test.go
@@ -0,0 +1,23 @@
+package ansible
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestMixin_Build(t *testing.T) {
+	m := NewTestMixin(t)
+
+	err  := m.Build()
+	require.NoError(t, err, "Build failed")
+
+	gotOutput := m.TestContext.GetOutput()
+
+	wantOutput := `RUN pip install --upgrade --no-cache-dir pip && \
+    pip install --upgrade --no-cache-dir setuptools wheel && \
+    pip install --upgrade --no-cache-dir 'ansible'
+    `
+
+	assert.Equal(t, wantOutput, gotOutput)
+}
