1030ec8e796be14b2209a34c293d280cc5ced71a
LvffY <louberger@hotmail.fr>
2022-02-21 18:26:09 +0000

    ✅ Try to add unit tests with non defaults values

--- pkg/ansible/build_test.go
+++ pkg/ansible/build_test.go
@@ -6,10 +6,10 @@ import (
 	"testing"
 )
 
-func TestMixin_Build(t *testing.T) {
+func TestMixinBuildWithDefaults(t *testing.T) {
 	m := NewTestMixin(t)
 
-	err  := m.Build()
+	err := m.Build()
 	require.NoError(t, err, "Build failed")
 
 	gotOutput := m.TestContext.GetOutput()
@@ -21,3 +21,25 @@ func TestMixin_Build(t *testing.T) {
 
 	assert.Equal(t, wantOutput, gotOutput)
 }
+
+// TODO: Add unit tests with some non defaults values
+/* func TestMixinBuildWithoutDefault(t *testing.T) {
+	m := NewTestMixin(t)
+
+	m.ClientVersion = "<2.10"
+	m.RequirementsFiles = []string{"requirements.txt", "http://host.com/requirements.txt"}
+	m.ConstraintsFiles = []string{"constraints.txt", "http://host.com/constraints.txt"}
+	m.OtherPipDependencies = []string{"aPythonDep", "anotherPythonDep<specificVersion"}
+
+	err := m.Build()
+	require.NoError(t, err, "Build failed")
+
+	gotOutput := m.TestContext.GetOutput()
+
+	wantOutput := `RUN pip install --upgrade --no-cache-dir pip && \
+	pip install --upgrade --no-cache-dir setuptools wheel && \
+	pip install --upgrade --no-cache-dir 'ansible<2.10' 'aPythonDep' 'anotherPythonDep<specificVersion' --requirement 'requirements.txt' --requirement 'http://host.com/requirements.txt' --constraint 'constraints.txt' --constraint 'http://host.com/constraints.txt'
+	`
+
+	assert.Equal(t, wantOutput, gotOutput)
+} */
